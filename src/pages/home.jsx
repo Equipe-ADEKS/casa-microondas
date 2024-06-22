7e3de6d5920209c4c0d76dd59d435cdb88d93b5d
vectorjoh <j.v.costacristo@gmail.com>
2024-06-22 00:14:22 -0300

    mais responsividade

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -13,54 +13,17 @@ const Home = () => {
 		</div>
 		<h2 className="title_carousel">Marcas com que trabalhamos</h2>
 		<Gallery />
-		<section className="shop">
-			<div className="shop_content">
-				<img src="./shop/shop_cadeira.jpg" alt=""/>
-				<div className="shop_text">
-					<span>Mais Vendidos</span>
-					<h2>Cadeira de jantar formada</h2>
-					<p>R$ 268,00</p>
-					<div className="ret"><button type="submit">Adicionar ao carrinho</button></div>
-				</div>
-			</div>
-			<div className="shop_content">
-				<img src="./shop/shop_ampulheta.jpg" alt=""/>
-				<div className="shop_text">
-					<span>Mais Vendidos</span>
-					<h2>Ampulheta de Areia Clarevelas</h2>
-					<p>R$ 18,00</p>
-					<div className="ret"><button type="submit">Adicionar ao carrinho</button></div>
-				</div>
-			</div>
-			<div className="shop_content">
-				<img src="./shop/shop_lampada.jpg" alt=""/>
-				<div className="shop_text">
-					<span>Mais Vendidos</span>
-					<h2>Candeeiro pendente intemporal</h2>
-					<p>R$ 18,00</p>
-					<div className="ret"><button type="submit">Adicionar ao carrinho</button></div>
-				</div>
-			</div>
-			<div className="shop_content">
-				<img src="./shop/shop_quadro.jpg" alt=""/>
-				<div className="shop_text">
-					<span>Mais Vendidos</span>
-					<h2>Arte de folhas emolduradas</h2>
-					<p>R$ 36,00</p>
-					<div className="ret"><button type="submit">Adicionar ao carrinho</button></div>
-				</div>
-			</div>
-		</section>
+		
 		<section className="services">
 			<div className='services_desc'>
 				<img src="./conserto-microondas.jpg" alt="" />
 				<p>Conserto microondas</p>
 			</div>
-			<div className='service_desc'> 
+			<div className='services_desc'> 
 				<img src="./conserto-forno-eletrico.jpg" alt="" />
 				<p>Conserto Forno Elétrico</p>
 			</div>
-			<div className='service_desc'>
+			<div className='services_desc'>
 				<img src="./refora-microondas.jpg" alt="" />
 				<p>Reforma Microondas</p>
 			</div>
@@ -70,32 +33,25 @@ const Home = () => {
 				<h3>Nosso mantra</h3>
 				<p className="author_content">“Atender bem para atender sempre"</p>
 				<div className="bio">
-					<img src="https://heim.nordicmade.com/wp-content/uploads/2023/04/profile.jpg" alt=""/>
+					<img src="./logo-preta.png" alt=""/>
 					<div className="bio_content">
-						<h3>Matt Wilkinson, founder of Heim</h3>
-						<p>Read more About Us</p>
+						<h3>Fundador da Casa do Microondas</h3>
 					</div>
 				</div>
 			</div>
 		</section>
-		<section className="info">
-			<div className="info_coluna">
-				<a href="./html/error.html">
-                    <img className="info_img" src="./acessorio.jpg" alt=""/>
-                    <p>Acessórios</p>
-                </a>
+		<section className="services">
+			<div className='services_desc'>
+				<img src="./Busca-e-entrega.jpg" alt="" />
+				<p>Busca e entrega</p>
 			</div>
-			<div className="info_coluna">
-				<a href="./html/error.html">
-                    <img className="info_img" src="./venda-microondas.jpg" alt=""/>
-                    <p>Venda microondas</p>
-                </a>
+			<div className='services_desc'> 
+				<img src="./venda-microondas.jpg" alt="" />
+				<p>Venda microondas</p>
 			</div>
-			<div className="info_coluna">
-				<a href="./html/error.html">
-                    <img className="info_img" src="./busca-e-entrega.jpg" alt=""/>
-                    <p>Busca e entrega</p>
-                </a>
+			<div className='services_desc'>
+				<img src="./acessorio.jpg" alt="" />
+				<p>Acessório</p>
 			</div>
 		</section>
 	</main>
